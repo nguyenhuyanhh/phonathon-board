9c3fa56c4e4c30c1d98f5e018cfa5829ced9fccf
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2017-11-29 15:41:51 +0800

    Sort callers based on no of pledges before displaying

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ function Caller(name) {
     // define a Caller
     this.name = name;
     this.pledges = [];
+    this.pledgeCount = 0;
+    this.pledgeCounts = { 1: 0, 2: 0, 3: 0 };
 }
 
 Caller.prototype = {
@@ -17,6 +19,8 @@ Caller.prototype = {
     addPledge: function (pledge) {
         // Add a pledge to the caller's pledges
         this.pledges.push(pledge);
+        this.pledgeCount++;
+        this.pledgeCounts[pledge.type]++; // update the count
     },
     pledgeToStr: function () {
         // String representation of pledge list
@@ -27,11 +31,41 @@ Caller.prototype = {
         });
         return plgs.join(",")
     },
+    pledgeCountToStr: function () {
+        // String representation of pledge counts
+        countPl = this.pledgeCounts[TYPE_PL];
+        countCc = this.pledgeCounts[TYPE_CC];
+        countGr = this.pledgeCounts[TYPE_GR];
+        return (countPl + " pledges, " + countCc + " credits, " + countGr + " GIROs: ");
+    },
     toStr: function () {
         // String representation of the Caller object
         // e.g. Caller X getting (P)100,(C)50,(G)10
-        return ("Caller " + this.name + " getting " + this.pledgeToStr());
+        return ("Caller " + this.name + " getting " + this.pledgeCountToStr() + this.pledgeToStr());
     }
+}
+
+function compareCaller(caller1, caller2) {
+    // compare callers based on number of pledges, then name
+    if (caller1.pledgeCount != caller2.pledgeCount) {
+        return caller1.pledgeCount - caller2.pledgeCount;
+    }
+    if (caller1.pledgeCounts[TYPE_GR] != caller2.pledgeCounts[TYPE_GR]) {
+        return caller1.pledgeCounts[TYPE_GR] - caller2.pledgeCounts[TYPE_GR];
+    }
+    if (caller1.pledgeCounts[TYPE_CC] != caller2.pledgeCounts[TYPE_CC]) {
+        return caller1.pledgeCounts[TYPE_CC] - caller2.pledgeCounts[TYPE_CC];
+    }
+    if (caller1.pledgeCounts[TYPE_PL] != caller2.pledgeCounts[TYPE_PL]) {
+        return caller1.pledgeCounts[TYPE_PL] - caller2.pledgeCounts[TYPE_PL];
+    }
+    if (caller1.name < caller2.name) {
+        return 1;
+    }
+    if (caller1.name > caller2.name) {
+        return -1;
+    }
+    return 0;
 }
 
 function Pledge(amount, type) {
@@ -130,6 +164,8 @@ const CLS_PLDG = "col-pledge col-xs-2 col-sm-2 col-md-2 col-lg-2";
 
 function output() {
     // HTML representation of the data
+
+    callers = callers.sort(compareCaller).reverse(); // sort callers
     callers.forEach(caller => {
         // Caller's outer row
         var row = document.createElement("div");
