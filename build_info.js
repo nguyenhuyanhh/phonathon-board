19c5d2c515d54fafe19301f483f4a7a91ddec539
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2018-02-02 13:57:09 +0800

    Improved build_info

--- build_info.js
+++ build_info.js
@@ -1,16 +1,32 @@
 function getBuildInfo() {
-    // Get build information from GitHub
+    // Counting GitHub commits - get the diff between
+    // latest and initial commits, then plus 1
+    var initSHA = "dabfd3352d2779b94fd01048d334b0c4d8c91c96";
+    var latestRequest = new XMLHttpRequest();
+    var buildRequest = new XMLHttpRequest();
 
-    // Request info via GitHub REST API v3
-    var request = new XMLHttpRequest();
-    request.open("GET", "https://api.github.com/repos/nguyenhuyanhh/phonathon-board/contributors", true);
-    request.send();
+    // Request latest commit via GitHub REST API v3
+    latestRequest.open("GET", "https://api.github.com/repos/nguyenhuyanhh/phonathon-board/git/refs/heads/master", true);
+    latestRequest.send();
 
-    // Parse response
-    request.onreadystatechange = function () {
+    // Parse latest commit
+    latestRequest.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
-            var response = JSON.parse(request.responseText);
-            var buildNo = response[0]["contributions"];
+            var response = JSON.parse(latestRequest.responseText);
+            var latestSHA = response["object"]["sha"];
+            console.log("Latest commit: " + latestSHA);
+
+            // Request build number
+            buildRequest.open("GET", "https://api.github.com/repos/nguyenhuyanhh/phonathon-board/compare/" + initSHA + "..." + latestSHA, true);
+            buildRequest.send();
+        }
+    }
+
+    // Parse build number
+    buildRequest.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var response = JSON.parse(buildRequest.responseText);
+            var buildNo = response["total_commits"] + 1;
             console.log("Current build: " + buildNo);
             document.getElementById("build").innerHTML = "v" + buildNo;
         }
