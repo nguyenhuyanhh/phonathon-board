9fa6cc1b469863b4420045e34915783f39b27e40
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2019-03-22 16:19:34 +0800

    Javascript indent 2 paces by default

--- js/pledges.js
+++ js/pledges.js
@@ -3,210 +3,220 @@
 var board = new Board();
 
 function loadBoard() {
-    // load the spreadsheet using Tabletop
-    console.debug("Refreshing data from spreadsheet.");
-    board.clear();
-    Tabletop.init({
-        key: URL,
-        wanted: ["Settings", "Pledges"],
-        callback: processBoard,
-    });
+  // load the spreadsheet using Tabletop
+  console.debug("Refreshing data from spreadsheet.");
+  board.clear();
+  Tabletop.init({
+    key: URL,
+    wanted: ["Settings", "Pledges"],
+    callback: processBoard
+  });
 }
 
 function pledgesFromStr(caller, str, type, special) {
-    // get the pledges from a str, add to a Caller
-    var order = 0;
-    var pledges = str.split(",");
-    pledges.forEach(element => {
-        if (special) {
-            // special mode: pledge "amount" could be anything
-            caller.addPledge(new BoardPledge(element, type, order));
-        }
-        else {
-            // normal mode: pledges must be valid
-            if (!isNaN(element)) {
-                caller.addPledge(new BoardPledge(parseInt(element), type, order));
-            }
-        }
-        order++;
-    });
+  // get the pledges from a str, add to a Caller
+  var order = 0;
+  var pledges = str.split(",");
+  pledges.forEach(element => {
+    if (special) {
+      // special mode: pledge "amount" could be anything
+      caller.addPledge(new BoardPledge(element, type, order));
+    } else {
+      // normal mode: pledges must be valid
+      if (!isNaN(element)) {
+        caller.addPledge(new BoardPledge(parseInt(element), type, order));
+      }
+    }
+    order++;
+  });
 }
 
 function parseSettings(data) {
-    // Parse setting from sheet "Settings"
-    var settings = {};
-    settings.special_mode = (data.special_mode == "0") ? false : true;
-    settings.maintenance = (data.maintenance == "0") ? false : true;
-    settings.manual_refresh = (data.manual_refresh == "0") ? false : true;
-    settings.two_col = (data.two_col == "0") ? false : true;
-    return settings;
+  // Parse setting from sheet "Settings"
+  var settings = {};
+  settings.special_mode = data.special_mode == "0" ? false : true;
+  settings.maintenance = data.maintenance == "0" ? false : true;
+  settings.manual_refresh = data.manual_refresh == "0" ? false : true;
+  settings.two_col = data.two_col == "0" ? false : true;
+  return settings;
 }
 
 function parseBoard(data, settings) {
-    // Parse board from sheet "Pledges"
-    for (var i = 0; i < data.length; i++) {
-        // get caller
-        var caller = new BoardCaller(data[i].Caller);
+  // Parse board from sheet "Pledges"
+  for (var i = 0; i < data.length; i++) {
+    // get caller
+    var caller = new BoardCaller(data[i].Caller);
 
-        // get pledges
-        if (data[i].Pledge) {
-            pledgesFromStr(caller, data[i].Pledge, TYPE_PL, settings.special_mode);
-        }
-        if (data[i].CC) {
-            pledgesFromStr(caller, data[i].CC, TYPE_CC, settings.special_mode);
-        }
-        if (data[i].GIRO) {
-            pledgesFromStr(caller, data[i].GIRO, TYPE_GR, settings.special_mode);
-        }
-        board.callers.push(caller);
+    // get pledges
+    if (data[i].Pledge) {
+      pledgesFromStr(caller, data[i].Pledge, TYPE_PL, settings.special_mode);
     }
+    if (data[i].CC) {
+      pledgesFromStr(caller, data[i].CC, TYPE_CC, settings.special_mode);
+    }
+    if (data[i].GIRO) {
+      pledgesFromStr(caller, data[i].GIRO, TYPE_GR, settings.special_mode);
+    }
+    board.callers.push(caller);
+  }
 }
 
 function processBoard(data, tabletop) {
-    // Parse the spreadsheet
-    var boardSheet = tabletop.sheets("Pledges");
-    var settingSheet = tabletop.sheets("Settings");
+  // Parse the spreadsheet
+  var boardSheet = tabletop.sheets("Pledges");
+  var settingSheet = tabletop.sheets("Settings");
 
-    var settings = parseSettings(settingSheet.all()[0]);
-    parseBoard(boardSheet.all(), settings);
+  var settings = parseSettings(settingSheet.all()[0]);
+  parseBoard(boardSheet.all(), settings);
 
-    // output
-    clearBoard();
-    outputBoard(settings);
+  // output
+  clearBoard();
+  outputBoard(settings);
 }
 
 // Style constants for one-column layout
-const CLS_NAME_ONLY = "col-caller col-3 col-lg-2 col-offset-right-9 col-lg-offset-right-10";
+const CLS_NAME_ONLY =
+  "col-caller col-3 col-lg-2 col-offset-right-9 col-lg-offset-right-10";
 const CLS_NAME_WITH_PL = "col-caller col-3 col-lg-2";
 const CLS_PLEDGES_WITH_PL = "col-9 col-lg-10 row";
 const CLS_PLEDGES = "col-12 col-md-6 col-lg-4 col-xl-3";
 const CLS_PLDG = "col-pledge col-2";
 
 // Style constants for two-column layout
-const CLS_NAME_ONLY_2 = "col-caller col-6 col-lg-4 col-offset-right-6 col-lg-offset-right-8";
+const CLS_NAME_ONLY_2 =
+  "col-caller col-6 col-lg-4 col-offset-right-6 col-lg-offset-right-8";
 const CLS_NAME_WITH_PL_2 = "col-caller col-6 col-lg-4";
 const CLS_PLEDGES_WITH_PL_2 = "col-6 col-lg-8 row";
 const CLS_PLEDGES_2 = "col-12 col-lg-6 col-xl-4";
 const CLS_PLDG_2 = "col-pledge col-4";
 
 function clearBoard() {
-    // Clear board content before reloading
-    var divs = [
-        document.getElementById("main-div-one-col"),
-        document.getElementById("col-left"),
-        document.getElementById("col-right")
-    ];
-    divs.forEach(div => {
-        while (div.firstChild) {
-            div.removeChild(div.firstChild);
-        }
-    });
+  // Clear board content before reloading
+  var divs = [
+    document.getElementById("main-div-one-col"),
+    document.getElementById("col-left"),
+    document.getElementById("col-right")
+  ];
+  divs.forEach(div => {
+    while (div.firstChild) {
+      div.removeChild(div.firstChild);
+    }
+  });
 }
 
 function outputBoard(settings) {
-
-    function toHtml(pledge, settings) {
-        // return HTML div of a pledge
-        var div = document.createElement("div");
-        div.className = (settings.two_col) ? CLS_PLDG_2 : CLS_PLDG;
-        var subdiv = document.createElement("div");
-        subdiv.innerHTML = pledge.amount;
-        if (pledge.type == TYPE_PL) {
-            subdiv.className = "col-pldg col-pl";
-        }
-        if (pledge.type == TYPE_CC) {
-            subdiv.className = "col-pldg col-cc";
-        }
-        if (pledge.type == TYPE_GR) {
-            subdiv.className = "col-pldg col-gr";
-        }
-        div.appendChild(subdiv);
-        return div;
+  function toHtml(pledge, settings) {
+    // return HTML div of a pledge
+    var div = document.createElement("div");
+    div.className = settings.two_col ? CLS_PLDG_2 : CLS_PLDG;
+    var subdiv = document.createElement("div");
+    subdiv.innerHTML = pledge.amount;
+    if (pledge.type == TYPE_PL) {
+      subdiv.className = "col-pldg col-pl";
     }
-
-    function getRow(caller, settings) {
-        // return HTML div of a row
-
-        // Caller's outer row
-        var row = document.createElement("div");
-        row.className = "row-caller row";
-
-        // Caller's name
-        var colName = document.createElement("div");
-        colName.innerHTML = caller.name;
-
-        // Pledges' outer row
-        var colPledges = document.createElement("div");
-
-        // Pledges
-        if (caller.pledges.length === 0) {
-            // no pledges, just output caller name
-            colName.className = (settings.two_col) ? CLS_NAME_ONLY_2 : CLS_NAME_ONLY;
-            row.appendChild(colName);
-        }
-        else {
-            colName.className = (settings.two_col) ? CLS_NAME_WITH_PL_2 : CLS_NAME_WITH_PL;
-            colPledges.className = (settings.two_col) ? CLS_PLEDGES_WITH_PL_2 : CLS_PLEDGES_WITH_PL;
-
-            // split pledges in groups of 6
-            // each put into a row
-            if (settings.special_mode) {
-                var sortedPledges = caller.pledges.sort(comparePledgeOrder);
-            }
-            else {
-                sortedPledges = caller.pledges.sort(comparePledgeAmount);
-            }
-            var countRows = Math.floor(sortedPledges.length / 6);
-            for (var i = 0; i <= countRows; i++) {
-                // wrapping elements
-                var colPlgs = document.createElement("div");
-                colPlgs.className = (settings.two_col) ? CLS_PLEDGES_2 : CLS_PLEDGES;
-                var rowPlgs = document.createElement("div");
-                rowPlgs.className = "row";
-
-                // pledges
-                var plgs = sortedPledges.slice(i * 6, (i + 1) * 6);
-                plgs.forEach(plg => {
-                    rowPlgs.appendChild(toHtml(plg, settings));
-                });
-                colPlgs.appendChild(rowPlgs);
-                colPledges.appendChild(colPlgs);
-            }
-
-            row.appendChild(colName);
-            row.appendChild(colPledges);
-        }
-        return row;
+    if (pledge.type == TYPE_CC) {
+      subdiv.className = "col-pldg col-cc";
     }
-
-    if (settings.maintenance) {
-        // Render maintenance site
-        var maintDiv = document.createElement("div");
-        maintDiv.innerHTML = "The site is under maintenance, please come back later!";
-        maintDiv.className = "maint";
-        document.getElementById("main-div").appendChild(maintDiv);
+    if (pledge.type == TYPE_GR) {
+      subdiv.className = "col-pldg col-gr";
     }
-    else {
-        // HTML representation of the data
-        board.sort();
-        var i = 0;
-        board.callers.forEach(caller => {
-            console.debug(caller.toStr());
-            var row = getRow(caller, settings);
-            if (settings.two_col) {
-                var div = (i % 2 == 0) ? document.getElementById("col-left") : document.getElementById("col-right");
-            }
-            else {
-                div = document.getElementById("main-div-one-col");
-            }
-            div.appendChild(row);
-            i++;
+    div.appendChild(subdiv);
+    return div;
+  }
+
+  function getRow(caller, settings) {
+    // return HTML div of a row
+
+    // Caller's outer row
+    var row = document.createElement("div");
+    row.className = "row-caller row";
+
+    // Caller's name
+    var colName = document.createElement("div");
+    colName.innerHTML = caller.name;
+
+    // Pledges' outer row
+    var colPledges = document.createElement("div");
+
+    // Pledges
+    if (caller.pledges.length === 0) {
+      // no pledges, just output caller name
+      colName.className = settings.two_col ? CLS_NAME_ONLY_2 : CLS_NAME_ONLY;
+      row.appendChild(colName);
+    } else {
+      colName.className = settings.two_col
+        ? CLS_NAME_WITH_PL_2
+        : CLS_NAME_WITH_PL;
+      colPledges.className = settings.two_col
+        ? CLS_PLEDGES_WITH_PL_2
+        : CLS_PLEDGES_WITH_PL;
+
+      // split pledges in groups of 6
+      // each put into a row
+      if (settings.special_mode) {
+        var sortedPledges = caller.pledges.sort(comparePledgeOrder);
+      } else {
+        sortedPledges = caller.pledges.sort(comparePledgeAmount);
+      }
+      var countRows = Math.floor(sortedPledges.length / 6);
+      for (var i = 0; i <= countRows; i++) {
+        // wrapping elements
+        var colPlgs = document.createElement("div");
+        colPlgs.className = settings.two_col ? CLS_PLEDGES_2 : CLS_PLEDGES;
+        var rowPlgs = document.createElement("div");
+        rowPlgs.className = "row";
+
+        // pledges
+        var plgs = sortedPledges.slice(i * 6, (i + 1) * 6);
+        plgs.forEach(plg => {
+          rowPlgs.appendChild(toHtml(plg, settings));
         });
+        colPlgs.appendChild(rowPlgs);
+        colPledges.appendChild(colPlgs);
+      }
 
-        // Display the total
-        var totalAmount = board.getTotalAmount();
-        document.getElementById("summary").textContent = "Total Pledges: $" + totalAmount[1] + " | Total Credit Cards: $" + totalAmount[2] + " | Total GIROs: $" + totalAmount[3];
+      row.appendChild(colName);
+      row.appendChild(colPledges);
     }
+    return row;
+  }
+
+  if (settings.maintenance) {
+    // Render maintenance site
+    var maintDiv = document.createElement("div");
+    maintDiv.innerHTML =
+      "The site is under maintenance, please come back later!";
+    maintDiv.className = "maint";
+    document.getElementById("main-div").appendChild(maintDiv);
+  } else {
+    // HTML representation of the data
+    board.sort();
+    var i = 0;
+    board.callers.forEach(caller => {
+      console.debug(caller.toStr());
+      var row = getRow(caller, settings);
+      if (settings.two_col) {
+        var div =
+          i % 2 == 0
+            ? document.getElementById("col-left")
+            : document.getElementById("col-right");
+      } else {
+        div = document.getElementById("main-div-one-col");
+      }
+      div.appendChild(row);
+      i++;
+    });
+
+    // Display the total
+    var totalAmount = board.getTotalAmount();
+    document.getElementById("summary").textContent =
+      "Total Pledges: $" +
+      totalAmount[1] +
+      " | Total Credit Cards: $" +
+      totalAmount[2] +
+      " | Total GIROs: $" +
+      totalAmount[3];
+  }
 }
 
 const REFRESH_INTERVAL = 15000; // 15 seconds
@@ -219,21 +229,21 @@ var id = setInterval(loadBoard, REFRESH_INTERVAL);
 setInterval(clock, 1000);
 
 function clock() {
-    // Auto-refresh timer
-    document.getElementById("clock").textContent = "Refreshing in " + clk + "...";
-    if (clk == 1) {
-        clk = CLOCK_SIGNAL; // reset clock
-    } else {
-        clk--;
-    }
+  // Auto-refresh timer
+  document.getElementById("clock").textContent = "Refreshing in " + clk + "...";
+  if (clk == 1) {
+    clk = CLOCK_SIGNAL; // reset clock
+  } else {
+    clk--;
+  }
 }
 
 function manualRefresh() {
-    // Manual refresh
-    clearInterval(id);
-    clk = CLOCK_SIGNAL;
-    loadBoard();
-    id = setInterval(loadBoard, REFRESH_INTERVAL);
+  // Manual refresh
+  clearInterval(id);
+  clk = CLOCK_SIGNAL;
+  loadBoard();
+  id = setInterval(loadBoard, REFRESH_INTERVAL);
 }
 
 document.getElementById("button-refresh").onclick = manualRefresh;
--- js/timesheets.js
+++ js/timesheets.js
@@ -3,174 +3,203 @@
 var sheet = new Sheet();
 
 function loadTimesheets() {
-    // load the spreadsheet using Tabletop
-    console.debug("Refreshing data from spreadsheet.");
-    sheet.clear();
-    Tabletop.init({
-        key: URL,
-        wanted: ["Settings", "Callers", "Timesheets"],
-        callback: processTimesheets,
-    });
+  // load the spreadsheet using Tabletop
+  console.debug("Refreshing data from spreadsheet.");
+  sheet.clear();
+  Tabletop.init({
+    key: URL,
+    wanted: ["Settings", "Callers", "Timesheets"],
+    callback: processTimesheets
+  });
 }
 
 function parseCallers(data) {
-    // Parse callers from sheet "Callers"
-    for (var i = 0; i < data.length; i++) {
-        // get caller
-        var row = data[i];
-        var caller = new SheetCaller(row["Name"], row["Full Name"], row["Role"], row["IC"], row["Matric"], row["Email"]);
-        sheet.callers[row["Name"]] = caller;
-    }
+  // Parse callers from sheet "Callers"
+  for (var i = 0; i < data.length; i++) {
+    // get caller
+    var row = data[i];
+    var caller = new SheetCaller(
+      row["Name"],
+      row["Full Name"],
+      row["Role"],
+      row["IC"],
+      row["Matric"],
+      row["Email"]
+    );
+    sheet.callers[row["Name"]] = caller;
+  }
 }
 
 function parseShifts(data) {
-    // Parse shifts from the header row of sheet "Timesheets"
-    var year = (new Date()).getFullYear();
-    var shifts = {};
-    data.forEach(datetime_str => {
-        if (datetime_str != "Shift") {
-            // parse the date/time from str
-            // format: day/month start_time-end_time (1/5 1830-2130)
-            var shift = {};
-            var datetime = datetime_str.split(" ");
-            var date_str = datetime[0];
-            var time_str = datetime[1];
-            var d = date_str.split("/");
-            var t = time_str.split("-");
-            var month = parseInt(d[1]) - 1;
-            var day = parseInt(d[0]);
-            var start_time = new Date(year, month, day, parseInt(t[0].slice(0, 2)), parseInt(t[0].slice(2)));
-            var end_time = new Date(year, month, day, parseInt(t[1].slice(0, 2)), parseInt(t[1].slice(2)));
-            shift.year = year;
-            shift.month = month;
-            shift.day = day;
-            shift.start_time = start_time;
-            shift.end_time = end_time;
-            shift.sunday = (start_time.getDay() == 0) ? true : false;
-            shifts[datetime_str] = shift;
-        }
-    });
-    return shifts;
+  // Parse shifts from the header row of sheet "Timesheets"
+  var year = new Date().getFullYear();
+  var shifts = {};
+  data.forEach(datetime_str => {
+    if (datetime_str != "Shift") {
+      // parse the date/time from str
+      // format: day/month start_time-end_time (1/5 1830-2130)
+      var shift = {};
+      var datetime = datetime_str.split(" ");
+      var date_str = datetime[0];
+      var time_str = datetime[1];
+      var d = date_str.split("/");
+      var t = time_str.split("-");
+      var month = parseInt(d[1]) - 1;
+      var day = parseInt(d[0]);
+      var start_time = new Date(
+        year,
+        month,
+        day,
+        parseInt(t[0].slice(0, 2)),
+        parseInt(t[0].slice(2))
+      );
+      var end_time = new Date(
+        year,
+        month,
+        day,
+        parseInt(t[1].slice(0, 2)),
+        parseInt(t[1].slice(2))
+      );
+      shift.year = year;
+      shift.month = month;
+      shift.day = day;
+      shift.start_time = start_time;
+      shift.end_time = end_time;
+      shift.sunday = start_time.getDay() == 0 ? true : false;
+      shifts[datetime_str] = shift;
+    }
+  });
+  return shifts;
 }
 
 function parseCell(cell, col, shifts, supervisor) {
-    // Parse a non-header cell from sheet "Timesheets"
-    if (!cell || cell == "Supervisor") {
-        // empty/ useless cell
-        return;
-    }
-    var obj = shifts[col];
+  // Parse a non-header cell from sheet "Timesheets"
+  if (!cell || cell == "Supervisor") {
+    // empty/ useless cell
+    return;
+  }
+  var obj = shifts[col];
 
-    try {
-        // Parse the caller name and timing info
-        var caller_time = cell.trim().split("(");
-        if (caller_time.length == 1) {
-            // normal shifts
-            var caller = caller_time[0];
-            var start_time = new Date(obj.start_time);
-            var end_time = new Date(obj.end_time);
-        } else if (caller_time.length == 2) {
-            // different start time/ end time
-            caller = caller_time[0].trim();
-            var start_end_str = caller_time[1].trim().slice(0, 9);
-            var t = start_end_str.split("-");
-            start_time = new Date(obj.year, obj.month, obj.day, parseInt(t[0].slice(0, 2)), parseInt(t[0].slice(2)));
-            end_time = new Date(obj.year, obj.month, obj.day, parseInt(t[1].slice(0, 2)), parseInt(t[1].slice(2)));
-        } else {
-            throw "Invalid cell: " + cell;
-        }
-
-        // Check caller
-        if (sheet.callers.hasOwnProperty(caller)) {
-            var callerObj = sheet.callers[caller];
-        } else {
-            throw "Invalid caller " + caller;
-        }
-
-        // Assign supervisor
-        if (supervisor) {
-            if (callerObj.role != "Supervisor") {
-                shifts[col].supervisor = undefined;
-                throw "Not a supervisor: " + caller;
-            } else {
-                shifts[col].supervisor = caller;
-            }
-        }
-
-        // Adjust timing for supervisor
-        if (supervisor) {
-            if (!obj.sunday) {
-                // non-Sunday shifts, shift start time 15 mins earlier
-                start_time.setMinutes(start_time.getMinutes() - 15);
-            } else if (obj.start_time.getHours() == 16) {
-                // Sunday shifts, shift end time 15 mins later
-                end_time.setMinutes(end_time.getMinutes() + 15);
-            }
-        }
-
-        // Make SheetShift obj
-        if (supervisor) {
-            // Supervisor shift
-            var type = "Supervisor";
-            var sup = undefined;
-        } else if (callerObj.role == "Caller") {
-            // Caller shift
-            type = "Caller";
-            sup = obj.supervisor;
-        } else {
-            // Mentor shift
-            type = "Mentor";
-            sup = obj.supervisor;
-        }
-        callerObj.addShift(col, new SheetShift(type, start_time, end_time, sup));
-
-    } catch (error) {
-        console.error(error);
-        return;
+  try {
+    // Parse the caller name and timing info
+    var caller_time = cell.trim().split("(");
+    if (caller_time.length == 1) {
+      // normal shifts
+      var caller = caller_time[0];
+      var start_time = new Date(obj.start_time);
+      var end_time = new Date(obj.end_time);
+    } else if (caller_time.length == 2) {
+      // different start time/ end time
+      caller = caller_time[0].trim();
+      var start_end_str = caller_time[1].trim().slice(0, 9);
+      var t = start_end_str.split("-");
+      start_time = new Date(
+        obj.year,
+        obj.month,
+        obj.day,
+        parseInt(t[0].slice(0, 2)),
+        parseInt(t[0].slice(2))
+      );
+      end_time = new Date(
+        obj.year,
+        obj.month,
+        obj.day,
+        parseInt(t[1].slice(0, 2)),
+        parseInt(t[1].slice(2))
+      );
+    } else {
+      throw "Invalid cell: " + cell;
     }
 
+    // Check caller
+    if (sheet.callers.hasOwnProperty(caller)) {
+      var callerObj = sheet.callers[caller];
+    } else {
+      throw "Invalid caller " + caller;
+    }
+
+    // Assign supervisor
+    if (supervisor) {
+      if (callerObj.role != "Supervisor") {
+        shifts[col].supervisor = undefined;
+        throw "Not a supervisor: " + caller;
+      } else {
+        shifts[col].supervisor = caller;
+      }
+    }
+
+    // Adjust timing for supervisor
+    if (supervisor) {
+      if (!obj.sunday) {
+        // non-Sunday shifts, shift start time 15 mins earlier
+        start_time.setMinutes(start_time.getMinutes() - 15);
+      } else if (obj.start_time.getHours() == 16) {
+        // Sunday shifts, shift end time 15 mins later
+        end_time.setMinutes(end_time.getMinutes() + 15);
+      }
+    }
+
+    // Make SheetShift obj
+    if (supervisor) {
+      // Supervisor shift
+      var type = "Supervisor";
+      var sup = undefined;
+    } else if (callerObj.role == "Caller") {
+      // Caller shift
+      type = "Caller";
+      sup = obj.supervisor;
+    } else {
+      // Mentor shift
+      type = "Mentor";
+      sup = obj.supervisor;
+    }
+    callerObj.addShift(col, new SheetShift(type, start_time, end_time, sup));
+  } catch (error) {
+    console.error(error);
+    return;
+  }
 }
 
 function parseTimesheets(data, shifts) {
-    // Parse timesheets from sheet "Timesheets"
-    for (var i = 0; i < data.length; i++) {
-        var row = data[i];
-        var supervisor = (i == 0) ? true : false;
-        for (var col in row) {
-            var cell = row[col];
-            parseCell(cell, col, shifts, supervisor);
-        }
+  // Parse timesheets from sheet "Timesheets"
+  for (var i = 0; i < data.length; i++) {
+    var row = data[i];
+    var supervisor = i == 0 ? true : false;
+    for (var col in row) {
+      var cell = row[col];
+      parseCell(cell, col, shifts, supervisor);
     }
+  }
 }
 
 function displayTimesheets() {
-    // Display the timesheets
-    sheet = sheet.filter();
+  // Display the timesheets
+  sheet = sheet.filter();
 
-    // Populate callers
-    var sel = document.getElementById("sheet-caller");
-    for (var caller in sheet.callers) {
-        var opt = document.createElement("option");
-        opt.value = caller;
-        opt.innerHTML = caller;
-        sel.appendChild(opt);
-    }
+  // Populate callers
+  var sel = document.getElementById("sheet-caller");
+  for (var caller in sheet.callers) {
+    var opt = document.createElement("option");
+    opt.value = caller;
+    opt.innerHTML = caller;
+    sel.appendChild(opt);
+  }
 }
 
 function processTimesheets(data, tabletop) {
-    // Parse the spreadsheet
-    parseCallers(tabletop.sheets("Callers").all());
+  // Parse the spreadsheet
+  parseCallers(tabletop.sheets("Callers").all());
 
-    var shifts = parseShifts(tabletop.sheets("Timesheets").columnNames);
-    parseTimesheets(tabletop.sheets("Timesheets").all(), shifts);
+  var shifts = parseShifts(tabletop.sheets("Timesheets").columnNames);
+  parseTimesheets(tabletop.sheets("Timesheets").all(), shifts);
 
-    // Display timesheets
-    displayTimesheets();
+  // Display timesheets
+  displayTimesheets();
 }
 
 function formHandler(f) {
-    var formData = new FormData(f.target);
-    console.log(formData);
+  var formData = new FormData(f.target);
+  console.log(formData);
 }
 
 window.addEventListener("DOMContentLoaded", loadTimesheets);
