01e83dd05a891bc0502d4d0acb259beea777b679
nguyenhuyanhh <nguyenhuyanhh@users.noreply.github.com>
2019-08-04 22:02:37 +0800

    Basic fetch from Google Sheets

--- src/index.js
+++ src/index.js
@@ -7,34 +7,60 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Summary from "./Summary";
 
-const data = [
-  {
-    caller: "Test",
-    cc: ["10", "10", "10"],
-    giro: ["10", "10", "10"],
-    pledge: ["10", "10", "10"]
-  },
-  {
-    caller: "Test2",
-    cc: ["10", "10", "10"],
-    giro: ["10", "10", "10"],
-    pledge: ["10", "10", "10"]
-  }
-];
+const URL =
+  "https://spreadsheets.google.com/feeds/cells/1btD0w0p58ZNJzSfDRGtCiZCCjvK80OkLKOA0j9YXbic/2/public/values?alt=json";
+const CLOCK = 30;
+
+function groupBy(arr, property) {
+  return arr.reduce(function(memo, x) {
+    if (!memo[x[property]]) {
+      memo[x[property]] = [];
+    }
+    memo[x[property]].push(x);
+    return memo;
+  }, {});
+}
+
+function parse(results) {
+  let cells = results.feed.entry.map(x => x["gs$cell"]);
+  let records = Object.values(groupBy(cells, "row")).slice(1);
+  return records.map(function(x) {
+    let caller = x.filter(f => f.col == 1);
+    let cc = x.filter(f => f.col == 2);
+    let pl = x.filter(f => f.col == 3);
+    let giro = x.filter(f => f.col == 4);
+    return {
+      caller: caller[0]["$t"].trim(),
+      cc: cc.length ? cc[0]["$t"].split(",") : cc,
+      pledge: pl.length ? pl[0]["$t"].split(",") : pl,
+      giro: giro.length ? giro[0]["$t"].split(",") : giro
+    };
+  });
+}
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { data: data, clock: 30 };
+    this.state = { data: [], clock: CLOCK };
+    this.update();
   }
 
   tick() {
     let clock = this.state.clock;
     if (clock === 1) {
-      this.setState({ clock: 30 });
+      this.setState({ clock: CLOCK });
+      this.update();
     } else {
       this.setState({ clock: clock - 1 });
     }
+  }
+
+  update() {
+    fetch(URL)
+      .then(res => res.json())
+      .then(results => {
+        this.setState({ data: parse(results) });
+      });
   }
 
   componentDidMount() {
